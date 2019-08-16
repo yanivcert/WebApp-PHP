847f900ab8cf53fbdc891d7248d29f4f8a10d9af
unknown <yanivcert@outlook.com>
2019-08-16 14:17:09 +0300

    Hello World

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-//echo '<h1>Hello World!</h1>';
-//echo '<img src="world.jpg">';
+echo '<h1>Hello World!</h1>';
+echo '<img src="world.jpg">';
 
-echo '<h1>Hello Sela Team!</h1>';
-echo '<img src="sela-logo.png">';
+//echo '<h1>Hello Sela Team!</h1>';
+//echo '<img src="sela-logo.png">';
 ?>
