d0dbca2a728ef3fab39207a268a6b40a20117dfa
unknown <yanivcert@outlook.com>
2019-08-16 14:03:37 +0300

    New Version

--- Jenkinsfile
+++ Jenkinsfile
@@ -18,7 +18,7 @@ pipeline {
     }
     post {
         success {
-            archiveArtifacts '*.php'
+            archiveArtifacts '*.*'
         }
     } 
 }
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 
-echo "Hello Sela Team!";
+echo '<h1>Hello World!</h1>';
+echo '<img src="world.jpg">';
+
+echo '<h1>Hello Sela Team!</h1>';
+echo '<img src="sela-logo.png">';
 ?>
